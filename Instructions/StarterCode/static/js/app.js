e98fcaa02bd6577195d094bd0a8effae02ba8997
wwright21 <williamcwrightjr@gmail.com>
2022-06-27 21:46:09 -0400

    updated homework

---
+++ Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,81 @@
+// // constant variable of the url to the data
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+
+// create the dropdown list
+
+function init() {
+
+  d3.json(url).then(function (data) {
+
+    console.log(data.metadata)
+
+    // totalIDs = []
+
+    for (i = 0; i < data.names.length; i++) {
+
+      // totalIDs.push(data.metadata[i].id)
+      let idList = d3.select("#selDataset");
+      namesList = idList.append("option");
+      namesList.text(data.names[i]);
+      // namesList.attr("value", data.names[i])
+
+    }
+
+    function buildTable(metadata) {
+      let demoTable = d3.select("#sample-metadata");
+      let fillTable = demoTable.append("table");
+      row = fillTable.append("tr");
+      tableData = row.append("td");
+      let ID = tableData.text('ID: ' + metadata.id)
+      row = fillTable.append("tr")
+      tableData = row.append("td")
+      let ethn = tableData.text('Ethnicity: ' + metadata.ethnicity)
+      row = fillTable.append("tr")
+      tableData = row.append("td")
+      let gender = tableData.text('Gender: ' + metadata.gender)
+      row = fillTable.append("tr")
+      tableData = row.append("td")
+      let age = tableData.text('Age: ' + metadata.age)
+      row = fillTable.append("tr")
+      tableData = row.append("td")
+      let location = tableData.text('Location: ' + metadata.location)
+      row = fillTable.append("tr")
+      tableData = row.append("td")
+      bbtype = tableData.text('bbtype: ' + metadata.bbtype)
+      row = fillTable.append("tr")
+      tableData = row.append("td")
+      wfreq = tableData.text('Wash frequency: ' + metadata.wfreq)
+    
+    }
+
+    buildTable(data.metadata[0])
+
+    
+
+    
+})
+
+}
+
+init();
+
+// for (i=0, i<data.metadata.length, i++) {
+//   function optionChanged()
+
+
+// }
+
+
+//  // optionChanged function is called in the index.html
+// function optionChanged(data.metadata) {
+//     // Print the Test Subject ID No. that prompted the change to the console
+//   console.log(data.metadata);
+//   // Call the Metadata function to change the display of the new demographic
+//   // data for the selected ID No. 
+//   Metadata(data.metadata);
+//   // Call the new Charts function to update the charts the relavant data for the
+//   // selected ID No. 
+//   newCharts(data.metadata);
+// }
+// // barChart(data.samples[0])
